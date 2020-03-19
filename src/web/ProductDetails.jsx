c43d6090134b149858cf200b41b9ab2d1591ddcb
Naijia Liu <naijialiu@geniesolutions.com.au>
2020-03-19 10:36:23 +1000

    show product details page

--- src/web/ProductDetails.jsx
+++ src/web/ProductDetails.jsx
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   shape, string, number, arrayOf, any,
 } from 'prop-types';
+import { useParams } from 'react-router-dom';
 import PriceChart from './PriceChart';
 
-function ProductDetails({ product }) {
+function ProductDetails() {
+  const { id } = useParams();
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    if (id) {
+      (async () => {
+        const response = await fetch(`https://pricehipster.com/api/search/products/byId?id=${id}&includeVariants=true`);
+        const data = await response.json();
+        setProduct(data.product);
+      })();
+    }
+  }, [id]);
+
   return (
     <>
       <h1>{product.name}</h1>
       <h5>{product.price}</h5>
+      <img src={product.image} alt={product.name} />
       <PriceChart priceHistory={product.priceHistory} price={product.price} />
     </>
   );
 }
 
 ProductDetails.propTypes = {
-  product: shape({
-    name: string,
-    price: number,
-    priceHistory: arrayOf(any),
-  }),
+  // product: shape({
+  //   name: string,
+  //   price: number,
+  //   priceHistory: arrayOf(any),
+  // }),
 };
 
 ProductDetails.defaultProps = {
-  product: {},
+  // product: {},
 };
 
 export default ProductDetails;
--- src/web/ProductList.jsx
+++ src/web/ProductList.jsx
@@ -6,18 +6,10 @@ import { Link } from 'react-router-dom';
 import { Flex, Box } from './components';
 
 function ProductList({ items, className }) {
-  function handleItemClick(item) {
-    return async function handler() {
-      const response = await fetch(`https://pricehipster.com/api/search/products/byId?id=${item.id}&includeVariants=true`);
-      const data = await response.json();
-      console.log(data);
-      // https://pricehipster.com/api/search/products/byId?id=QojADQNK8ZF9V8S_77RDBQ~HJ1Ki6pa~au&includeVariants=true
-    };
-  }
   return (
     <Flex className={className} direction="column">
       {items.map(item => (
-        <Flex key={item.id} onClick={handleItemClick(item)}>
+        <Flex key={item.id}>
           <img src={item.image} alt={item.name} flex="auto" />
           <Flex>
             <Link dangerouslySetInnerHTML={{ __html: item.name_highlighted }} to={`/products/${item.id}`} />
