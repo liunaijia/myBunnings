921a1b9c48a714b2a225cd2e96596d6b6e1fd1c6
Naijia Liu <naijialiu@geniesolutions.com.au>
2020-03-20 09:17:00 +1000

    use chakra theme provider

--- src/web/App.jsx
+++ src/web/App.jsx
@@ -1,41 +1,14 @@
 import React from 'react';
-import { string } from 'prop-types';
-import styled from 'styled-components';
-import GlobalStyle from './App.css';
+import { ThemeProvider, CSSReset } from '@chakra-ui/core';
 import Router from './Router';
 
-function App({ className }) {
+function App() {
   return (
-    <>
-      <GlobalStyle />
-      <main className={className}>
-        <Router />
-      </main>
-      <footer>
-        By Naijia
-      </footer>
-    </>
+    <ThemeProvider>
+      <CSSReset />
+      <Router />
+    </ThemeProvider>
   );
 }
 
-App.propTypes = {
-  className: string,
-};
-
-App.defaultProps = {
-  className: null,
-};
-
-export default styled(App)`
-  // display: flex;
-  // min-height: 100vh;
-
-  // article {
-  //   flex: 1;
-  // }
-
-  // aside {
-  //   width: auto;
-  //   background: #1e1e1d;
-  // }
-`;
+export default App;
