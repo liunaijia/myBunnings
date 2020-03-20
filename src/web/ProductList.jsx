1c75b1e960dc03b237967fa9085ec5c78b765197
Naijia Liu <naijialiu@geniesolutions.com.au>
2020-03-20 14:45:24 +1000

    embed price history into product list page

--- src/web/ProductList.jsx
+++ src/web/ProductList.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   string, arrayOf, shape, number,
 } from 'prop-types';
-import { Link } from 'react-router-dom';
 import {
   Flex, StatArrow, Box, Stack, Image, Text,
 } from '@chakra-ui/core';
+import PriceChart from './PriceChart';
 
 function ProductList({ items }) {
+  const [priceHistory, setPriceHistory] = useState({});
+  const [selectedItemId, setSelectedItemId] = useState();
+
+  function handleItemClick(itemId) {
+    return function handler() {
+      setSelectedItemId(itemId);
+    };
+  }
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`https://pricehipster.com/api/search/products/byId?id=${selectedItemId}&includeVariants=true`);
+      const data = await response.json();
+      setPriceHistory({ ...priceHistory, [selectedItemId]: data.product });
+    })();
+  }, [selectedItemId]);
+
   return (
     <Stack spacing={0}>
       {items.map(item => (
-        <Box borderTopWidth="10px" key={item.id} as={Link} to={`/products/${item.id}`}>
-          <Flex>
+        <Box borderTopWidth="10px" key={item.id}>
+          <Flex onClick={handleItemClick(item.id)}>
             <Image src={item.image} alt={item.name} size="32" m={1} />
             <Flex direction="column" justify="space-between" mt={4} mb={4}>
-              <Text dangerouslySetInnerHTML={{ __html: item.name_highlighted }} lineHeight="shorter" />
+              <Text lineHeight="shorter">{item.name}</Text>
               <Flex>
                 <Text color="red.500">
                   {`$${item.price}`}
@@ -31,6 +48,9 @@ function ProductList({ items }) {
               </Flex>
             </Flex>
           </Flex>
+          {priceHistory[item.id] && (
+            <PriceChart priceHistory={priceHistory[item.id].priceHistory} price={priceHistory[item.id].price} />
+          )}
         </Box>
       ))}
     </Stack>
@@ -39,7 +59,12 @@ function ProductList({ items }) {
 
 ProductList.propTypes = {
   items: arrayOf(shape({
-    name_highlighted: string, id: string, image: string, name: string, price: number, priceDiff: number,
+    id: string,
+    image: string,
+    name: string,
+    price: number,
+    priceDiff: number,
+    normalPrice: number,
   })),
 };
 
--- src/web/Router.jsx
+++ src/web/Router.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import SearchResult from './SearchResult';
-import ProductDetails from './ProductDetails';
 
 function Router() {
   return (
@@ -10,7 +9,6 @@ function Router() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/search" component={SearchResult} />
-        <Route exact path="/products/:id" component={ProductDetails} />
       </Switch>
     </HashRouter>
   );
