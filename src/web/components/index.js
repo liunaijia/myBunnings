92394d44f1891ee7b35b5952505e2d8eabcd2ff5
Naijia Liu <naijialiu@geniesolutions.com.au>
2020-03-20 14:22:28 +1000

    remove needless files

--- src/web/components/index.js
+++ src/web/components/index.js
@@ -1,7 +1,3 @@
 export { default as ErrorBoundary } from './ErrorBoundary';
-export { default as Input } from './Input';
 export { default as Form } from './Form';
 export { default as useForm } from './useForm';
-export { default as Box } from './Box';
-export { default as Grid } from './Grid';
-export { default as Flex } from './Flex';
