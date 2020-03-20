094a47656ace8a03b893b39c287b2baeecd3da7a
Naijia Liu <naijialiu@geniesolutions.com.au>
2020-03-20 19:12:26 +1000

    add search header

--- src/web/SearchResult.jsx
+++ src/web/SearchResult.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import {
-  Progress, Flex, Stack, Text,
+  Progress, Flex, Stack, Icon, Input, InputGroup, InputLeftElement,
 } from '@chakra-ui/core';
 import ProductList from './ProductList';
 
 function SearchResult() {
   const [searchResult, setSearchResult] = useState();
 
+  const history = useHistory();
+
   const query = new URLSearchParams(useLocation().search);
   const q = query.get('q');
+
+  const [keyword, setKeyword] = useState(q);
 
   useEffect(() => {
     if (q) {
@@ -23,10 +27,23 @@ function SearchResult() {
     }
   }, [q]);
 
+  function handleQueryChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  function handleSubmit() {
+    history.push(`/search?q=${keyword}`);
+  }
+
   if (searchResult) {
     return (
       <>
-        <Text m={4}>{searchResult.matchedProductName}</Text>
+        <Flex position="sticky" top={0} bg="#4154b3" p={4} as="form" onSubmit={handleSubmit}>
+          <InputGroup color="white" flex="auto">
+            <InputLeftElement><Icon name="search" /></InputLeftElement>
+            <Input name="q" bg="#5465bb" borderWidth={0} size="lg" value={keyword} onChange={handleQueryChange} />
+          </InputGroup>
+        </Flex>
         <Stack spacing={4}>
           <ProductList items={searchResult.matchedProducts.products} />
         </Stack>
