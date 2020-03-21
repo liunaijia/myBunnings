8d6e643af33e27fe78af6ab6b4b4673008a44281
Naijia Liu <naijialiu@geniesolutions.com.au>
2020-03-21 10:10:14 +1000

    publish

--- src/web/service.js
+++ src/web/service.js
@@ -4,8 +4,8 @@ export async function findProducts(q, { sort, start, length }) {
   const params = {
     q, sort, start, length,
   };
-  // const apiEndpoint = 'https://lugc11fh1k.execute-api.ap-southeast-2.amazonaws.com/prod';
-  const apiEndpoint = 'http://localhost:3000';
+  const apiEndpoint = 'https://lugc11fh1k.execute-api.ap-southeast-2.amazonaws.com/prod';
+  // const apiEndpoint = 'http://localhost:3000';
   const response = await fetch(`${apiEndpoint}/products?${toQueryString(params)}`);
   return response.json();
 }
